0012d68a460d6d2b03648bc874449d526dd5488b
Mame-Radia <mameradia22@gmail.com>
2022-04-03 09:30:27 +0300

    initialhgmmk

--- backend/src/config.js
+++ backend/src/config.js
@@ -14,7 +14,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 500,
+    growEditionSizeTo: 5000,
     layersOrder: [
       { name: "Chest Cover" },
       { name: "Hair" },
